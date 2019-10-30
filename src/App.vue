1f6578829e885901c886150100529a710ee24862
Igor de Paula <principe.borodin@gmail.com>
2019-10-30 10:46:28 -0300

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,115 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <button id="authorize_button" v-if="auth" @click="handleAuthClick">Authorize</button>
+        <button id="signout_button" v-if="!auth" @click="handleSignoutClick">Sign Out</button>
+        <ul>
+            <li v-for="item in docs" @click="getThumbnail(item.id)" :key="item.id">{{item.name}}</li>
+        </ul>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    export default {
+        name: 'app',
+        components: {},
+        data() {
+            return {
+                auth: true,
+                apiKey: 'AIzaSyCTrU266mWLQjD9D4NyGUQl9CY3Tm-idgA',
+                clientId: '284342642848-9iq27f5v971epnr6qiuvktoh2tduqfjc.apps.googleusercontent.com',
+                discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest','https://slides.googleapis.com/$discovery/rest?version=v1'],
+                scope: 'https://www.googleapis.com/auth/presentations',
+                docs: [],
+                thumbnails: []
+            }
+        },
+        mounted() {
+            const script = document.createElement('script')
+            const head = document.querySelector('head')
+            script.src = 'https://apis.google.com/js/api.js'
+            script.onload = () => {
+                console.log('carregado')
+                this.handleClientLoad()
+            }
+            head.appendChild(script)
+        },
+        methods: {
+            initClient() {
+                console.log('iniciando')
+                gapi.client.init({
+                    apiKey: this.apiKey,
+                    clientId: this.clientId,
+                    discoveryDocs: this.discoveryDocs,
+                    scope: this.scope
+                }).then(() => {
+                    // Listen for sign-in state changes.
+                    gapi.auth2.getAuthInstance().isSignedIn.listen(this.updateSigninStatus);
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+                    // Handle the initial sign-in state.
+                    this.updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+
+                })
+                    .catch(console.error);
+            },
+            updateSigninStatus(isSignedIn) {
+                if (isSignedIn) {
+                    this.auth = false
+                    this.printDocTitle()
+                } else {
+                    this.auth = true
+                }
+            },
+            handleAuthClick(event) {
+                gapi.auth2.getAuthInstance().signIn();
+            },
+            handleSignoutClick(event) {
+                gapi.auth2.getAuthInstance().signOut();
+                this.docs = []
+            },
+            handleClientLoad() {
+                gapi.load('client:auth2', this.initClient);
+            }, printDocTitle() {
+                gapi.client.drive.files.list({
+
+                    'fields': "nextPageToken, files(id, name,  mimeType)"
+                }).then(response => {
+                    this.docs = response.result.files.filter(item => item.mimeType == 'application/vnd.google-apps.presentation');
+                });
+
+            },
+            getThumbnail(slideId){
+                gapi.client.slides.presentations.get({
+                    presentationId: slideId
+                }).then(function(response) {
+                    console.log('response 1',response.result.slides)
+                    response.result.slides.forEach(slide =>{
+                        gapi.client.slides.presentations.pages.getThumbnail({
+                            presentationId: slideId,
+                            pageObjectId: slide.objectId
+                        }).then(function(response) {
+                            console.log('response 2',response)
+
+                        }, function(response) {
+                           consonle.error('Error: ' + response.result.error.message);
+                        });
+                    })
+
+                }, function(response) {
+                    consonle.error('Error: ' + response.result.error.message);
+                });
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+import VueGAPI from "vue-gapi";
 
+// create the 'options' object
+const apiConfig = {
+  apiKey: "AIzaSyCTrU266mWLQjD9D4NyGUQl9CY3Tm-idgA",
+  clientId: "284342642848-v9v15bmo0dvmqoh9ejp2krp19kc5dh2v.apps.googleusercontent.com",
+  discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
+  scope: "https://www.googleapis.com/auth/presentations"
+  // see all available scopes here: https://developers.google.com/identity/protocols/googlescopes'
+};
+
+// Use the plugin and pass along the configuration
+Vue.use(VueGAPI, apiConfig);
 new Vue({
   render: h => h(App),
 }).$mount('#app')
